49ba3c444196f386ac709e54950784b9bf51ba80
Frederico Ferraz <frodrigues@ccpr.coop.br>
2025-04-28 14:30:46 -0300

    Adiciona EF Core e cria migração inicial do banco
    Foram adicionadas referências aos pacotes `Microsoft.EntityFrameworkCore.Design` e `Microsoft.EntityFrameworkCore.Tools` (v9.0.4) nas camadas `Integracao.Application`, `Integracao.Domain`, `Integracao.Infra`, `Integracao.WebApi.Endpoints` e `Integracao.WebApi`, com configurações de escopo restrito (`PrivateAssets` e `IncludeAssets`).
    
    Foi criada a migração `20250428172722_CriacaoTabelas` na camada `Integracao.Infra`, que define a criação das tabelas `TDE_PARA_STATUS`, `TTICKETS` e `TUSUARIOS`, representando entidades de status, tickets e usuários.
    
    O arquivo `20250428172722_CriacaoTabelas.Designer.cs` foi gerado para mapear as entidades ao banco, e o `SqlServerDbContextModelSnapshot.cs` foi atualizado para refletir o modelo atual.
    
    Essas mudanças habilitam o uso do Entity Framework Core como ORM no projeto, estruturando a interação com o banco de dados.

---
+++ Integracao.Infra/Migrations/20250428172722_CriacaoTabelas.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Integracao.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TDE_PARA_STATUS",
+                columns: table => new
+                {
+                    ACOD_STATUS = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ACOD_STATUS_Glpi = table.Column<int>(type: "int", nullable: false),
+                    ADES_STATUS_GLPI = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    ACOD_STATUS_DEVOPS = table.Column<int>(type: "int", nullable: false),
+                    ADES_STATUS_DEVOPS = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TDE_PARA_STATUS", x => x.ACOD_STATUS);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TTICKETS",
+                columns: table => new
+                {
+                    ACOD_TICKET = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ACOD_TICKET_GLPI = table.Column<long>(type: "bigint", nullable: false),
+                    ACOD_TICKET_DEVOPS = table.Column<long>(type: "bigint", nullable: false),
+                    ACOD_STATUS = table.Column<long>(type: "bigint", nullable: false),
+                    ADAT_ALTERACAO = table.Column<DateTime>(type: "datetime", nullable: false),
+                    ACOD_USUARIO = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TTICKETS", x => x.ACOD_TICKET);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TUSUARIOS",
+                columns: table => new
+                {
+                    ACOD_USUARIO = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ADES_EMAIL = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true),
+                    ADES_USUARIO = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TUSUARIOS", x => x.ACOD_USUARIO);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TDE_PARA_STATUS");
+
+            migrationBuilder.DropTable(
+                name: "TTICKETS");
+
+            migrationBuilder.DropTable(
+                name: "TUSUARIOS");
+        }
+    }
+}
