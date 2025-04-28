d1bb002e806b064020b5f7f955556b8a1dc02cf0
freddyferraz <68133605+freddyferraz@users.noreply.github.com>
2025-04-28 07:44:42 -0300

    Delete src directory

--- src/Integracao.Infra/Abstractions/IUsuarioRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Integracao.Infra.Entities;
-
-namespace Integracao.Infra.Abstractions;
-
-public interface IUsuarioRepository
-{
-    ValueTask<(IEnumerable<Usuario> data, int total)> RetornaUsuarioById(long id);
-}
